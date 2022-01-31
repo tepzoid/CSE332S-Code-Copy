c7643f765210ec5058817f99dc94f7ee5c5b0f95
c.jinfeng <c.jinfeng@wustl.edu>
2022-01-30 22:26:26 -0600

    Jan. 30 Partial Review Studio 3

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -3,12 +3,19 @@
 
 #include "CMakeProject1.h"
 
+
 using namespace std;
 
-int main()
+int main(int, char * argv[])
 {
-	cout << "Hello CMake." << endl;
-    //hiding information here
-	//reverse the process
+
+    const char * p = argv[0]; //Why not & argv[0]?
+
+    while(p!='\0'){           //Why is the output like that? Isn't char * represent C-style strings?
+        cout << *p << endl;
+        ++p;
+    }
+
+   // cout << argv[0]<<endl;
 	return 0;
 }
